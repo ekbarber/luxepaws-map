328fc58f547eefcee0b0b885d7978c96538268de
Eddie <ed@edkbarber.com>
2025-01-05 11:39:38 -0800

    wip: simple angular/primeng started

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        AppComponent
-      ],
-      providers: [provideRouter([])]
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -18,9 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'CoreUI Angular Admin Template'`, () => {
+  it(`should have the 'luxepaws' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CoreUI Angular Admin Template');
+    expect(app.title).toEqual('luxepaws');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, luxepaws');
   });
 });
