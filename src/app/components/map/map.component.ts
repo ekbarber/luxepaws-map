17475de3ddd0cdbb9d1c6804d7534af957522dc7
Eddie <ed@edkbarber.com>
2025-02-07 17:21:59 -0800

    feat: slider working

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -10,12 +10,9 @@ import { CatDataService, FixedCat } from '../../services/cat-data.service';
 import { Subscription } from 'rxjs';
 import _ from 'lodash';
 import { Card } from 'primeng/card';
-
-interface Location {
-  latitude: number;
-  longitude: number;
-  name: string;
-}
+import { Slider } from 'primeng/slider';
+import { FormsModule } from '@angular/forms';
+import { DateTime } from 'luxon';
 
 const TILE_LAYER = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const CENTER: [number, number] = [34.053718, -118.2452282];
@@ -25,19 +22,39 @@ const DEFAULT_ZOOM = 14;
   selector: 'map',
   template: `
     <p-card>
+      <div class="filter-controls">
+        <div class="slider-container">
+          <div class="date-labels">
+            <span>{{ getFormattedDate(selectedStartDate) }}</span>
+            <span>{{ getFormattedDate(selectedEndDate) }}</span>
+          </div>
+          <p-slider
+            [(ngModel)]="rangeValues"
+            [range]="true"
+            [min]="0"
+            [max]="100"
+            (onChange)="onRangeChange($event)"
+          >
+          </p-slider>
+        </div>
+      </div>
       <div id="map"></div>
     </p-card>
   `,
   styleUrls: ['./map.component.scss'],
   changeDetection: ChangeDetectionStrategy.Default,
   standalone: true,
-  imports: [Card],
+  imports: [Card, Slider, FormsModule],
 })
 export class MapComponent implements AfterContentInit, OnDestroy {
   private map!: L.Map;
   private catSubscription: Subscription;
   private markers: Array<L.Marker | L.CircleMarker> = [];
   private cats: FixedCat[] = [];
+  private dateRange: { start: DateTime; end: DateTime } | null = null;
+  rangeValues: number[] = [0, 100];
+  selectedStartDate: DateTime | null = null;
+  selectedEndDate: DateTime | null = null;
 
   constructor(
     private changeDetectorRef: ChangeDetectorRef,
@@ -45,9 +62,57 @@ export class MapComponent implements AfterContentInit, OnDestroy {
   ) {
     this.catSubscription = this.catDataService.cats$.subscribe((cats) => {
       this.cats = cats;
+      this.initializeDateRange(cats);
       if (this.map) {
-        this.updateMarkers(cats);
+        this.updateMarkers(this.filterCatsByDateRange(cats));
       }
+    });
+  }
+
+  private initializeDateRange(cats: FixedCat[]): void {
+    if (cats.length === 0) return;
+
+    const dates = cats.map((cat) => cat.dateIssued);
+
+    const start = _.min(dates) || DateTime.now();
+    const end = _.max(dates) || DateTime.now();
+    this.dateRange = { start, end };
+    this.updateSelectedDatesFromRange();
+  }
+
+  private updateSelectedDatesFromRange(): void {
+    if (!this.dateRange) return;
+
+    const { start, end } = this.dateRange;
+    const totalRange = end.diff(start).toMillis();
+
+    this.selectedStartDate = DateTime.fromMillis(
+      start.toMillis() + (this.rangeValues[0] / 100) * totalRange
+    );
+
+    this.selectedEndDate = DateTime.fromMillis(
+      start.toMillis() + (this.rangeValues[1] / 100) * totalRange
+    );
+  }
+
+  getFormattedDate(date: DateTime | null | undefined): string {
+    return date ? date.toLocaleString() : 'Select date';
+  }
+
+  onRangeChange(event: any): void {
+    this.updateSelectedDatesFromRange();
+    this.updateMarkers(this.filterCatsByDateRange(this.cats));
+  }
+
+  private filterCatsByDateRange(cats: FixedCat[]): FixedCat[] {
+    if (!this.selectedStartDate || !this.selectedEndDate) return cats;
+
+    return cats.filter((cat) => {
+      const catDate = cat.dateIssued;
+      return (
+        catDate >= this.selectedStartDate! &&
+        catDate <= this.selectedEndDate!
+      );
     });
   }
 
