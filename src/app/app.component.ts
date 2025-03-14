3601aa85fa8596c56eda09c3719a8cc6e0ecb328
ekbarber <ed@edkbarber.com>
2025-03-13 19:06:16 -0700

    chore: style updates (#1)
    * chore: improve style
    
    * feat: small layout tweaks
    
    * chore: more minor style updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CardModule } from 'primeng/card';
-import { ButtonModule } from 'primeng/button';
 import { MapComponent } from './components/map/map.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, CardModule, ButtonModule, MapComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [MapComponent],
 })
-export class AppComponent {
-  title = 'Luxe Paws TNR Statistics';
-}
+export class AppComponent {}
